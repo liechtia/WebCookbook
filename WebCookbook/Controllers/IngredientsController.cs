7e041859e8ac0d5f913ce1057e32e3e2ed5e96bc
Andrea Liechti <andrea_liechti@msn.com>
2015-11-14 15:59:45 +0100

    not working :S

---
+++ WebCookbook/Controllers/IngredientsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebCookbook.Models;
+
+namespace WebCookbook.Controllers
+{
+    public class IngredientsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Ingredients
+        public ActionResult IndexIngredient()
+        {
+            return View(db.Ingredients.ToList());
+        }
+
+        // GET: Ingredients/Details/5
+        public ActionResult DetailsIngredient(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Create
+        public ActionResult CreateIngredient()
+        {
+            return View();
+        }
+
+        // POST: Ingredients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateIngredient([Bind(Include = "IngredientId,Measurement,AmountPerInitialServing,IngredientName")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ingredients.Add(ingredient);
+                db.SaveChanges();
+                return RedirectToAction("IndexIngredient");
+            }
+
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Edit/5
+        public ActionResult EditIngredient(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredient);
+        }
+
+        // POST: Ingredients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditIngredient([Bind(Include = "IngredientId,Measurement,AmountPerInitialServing,IngredientName")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ingredient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("IndexIngredient");
+            }
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Delete/5
+        public ActionResult DeleteIngredient(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredient);
+        }
+
+        // POST: Ingredients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ingredient ingredient = db.Ingredients.Find(id);
+            db.Ingredients.Remove(ingredient);
+            db.SaveChanges();
+            return RedirectToAction("IndexIngredient");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebCookbook/Models/RecipesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebCookbook.Models
+{
+    public class RecipesViewModel
+    {
+        public Recipe Recipe { get; set; }
+        public IList<Ingredient> Ingredients { get; set; } 
+    }
+}
