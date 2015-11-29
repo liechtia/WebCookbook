a396961b05af3144081b254185cff43c3c0cc89b
Michael Zbinden <michael.zbinden@unifr.ch>
2015-11-29 15:51:23 +0100

    Seed now adds an admin user.

--- WebCookbook/Migrations/Configuration.cs
+++ WebCookbook/Migrations/Configuration.cs
@@ -19,29 +19,27 @@ namespace WebCookbook.Migrations
 
         protected override void Seed(WebCookbook.Models.ApplicationDbContext context)
         {
-            if(!context.Users.Any(u => u.UserName == "admin"))
+
+            context.Roles.AddOrUpdate(r => r.Name, new IdentityRole("admin"));
+            context.SaveChanges();
+            if (!context.Users.Any(x => x.UserName == "admin@admin.ch"))
             {
                 var store = new UserStore<ApplicationUser>(context);
                 var manager = new UserManager<ApplicationUser>(store);
-                var user = new ApplicationUser { UserName = "admin", Email = "admin@admin.ch" };
-                //IdentityRole role = new IdentityRole("admin");
-                //IdentityUserRole userRole = new IdentityUserRole();
-                //user.Roles.Add(userRole);
-                //role.Users.Add(userRole);
-                //context.Roles.Add(role);
-                manager.Create(user, "password");
-                
+                var user = new ApplicationUser { UserName = "admin@admin.ch", Email = "admin@admin.ch" };
+
+                manager.Create(user, "Asdf+234");
+                manager.AddToRole(user.Id, "admin");
             }
+
             var IngredientsA = new List<Ingredient>();
             IngredientsA.Add(new Ingredient { AmountPerInitialServing = 150, Measurement = "mg", IngredientName = "Mehl" });
             var IngredientsB = new List<Ingredient>();
             IngredientsB.Add(new Ingredient { AmountPerInitialServing = 150, Measurement = "mg", IngredientName = "Mehl" });
             IngredientsB.Add(new Ingredient { AmountPerInitialServing = 4, Measurement = "Stück", IngredientName = "Eier" });
-            context.Recipes.AddOrUpdate(
-                r => r.Title,
-                new Recipe { Title = "Kuchen", InitialServings = 1, Instructions="Vermische alles" ,Ingredients = IngredientsA, User = context.Users.FirstOrDefault(u => u.UserName=="user")},
-                new Recipe { Title = "Crepes", InitialServings = 4, Instructions = "Vermische alles", Ingredients = IngredientsB, User = context.Users.FirstOrDefault(u => u.UserName == "user") }
-                );
+            context.Recipes.AddOrUpdate(r => r.Title,
+                new Recipe { Title = "Kuchen", InitialServings = 1, Instructions = "Vermische alles", Ingredients = IngredientsA, User = context.Users.FirstOrDefault(u => u.UserName == "admin@admin.ch") },
+                new Recipe { Title = "Crepes", InitialServings = 4, Instructions = "Vermische alles", Ingredients = IngredientsB, User = context.Users.FirstOrDefault(u => u.UserName == "admin@admin.ch") });
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
--- WebCookbook/Models/IdentityModels.cs
+++ WebCookbook/Models/IdentityModels.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Text;
 
 namespace WebCookbook.Models
 {
@@ -47,6 +49,59 @@ namespace WebCookbook.Models
             modelBuilder.Entity<Ingredient>()
                 .HasRequired(i => i.Recipe)
                 .WithMany(r => r.Ingredients);
+        }
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                var sb = new StringBuilder();
+
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                    ); // Add the original exception as the innerException
+            }
+        }
+        public override Task<int> SaveChangesAsync()
+        {
+            try
+            {
+                return base.SaveChangesAsync();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                var sb = new StringBuilder();
+
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                    ); // Add the original exception as the innerException
+            }
         }
     }
 }
